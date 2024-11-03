9245d99abe26f0aeb4fea0c9b56cee9d0eba5741
@maptaul <maptaulislam1@gmail.com>
2024-11-04 03:07:18 +0600

    f

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,10 @@
+import banner from "../assets/banner.jpg";
 const Banner = () => {
   return (
     <div className="w-2/4 h-96 relative z-10 mx-auto -mt-16 mb-10 flex justify-center items-center border-8 border-gray-300 rounded-lg ">
       <img
         className="w-full h-full  items-center justify-center"
-        src="/src/assets/banner.jpg"
+        src={banner}
         alt="Banner Image"
       />
     </div>
